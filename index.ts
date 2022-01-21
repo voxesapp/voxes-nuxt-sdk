62e7281a207068fcc148795fa1a1f330e309c455
Patryk Gulas <patrykgulas@Glovousers-MacBook-Pro.local>
2022-01-21 17:03:09 +0100

    fix sdk url

--- index.ts
+++ index.ts
@@ -1,29 +1,29 @@
-import { Module } from '@nuxt/types'
-import { MetaInfo } from 'vue-meta/types'
+import { Module } from "@nuxt/types";
+import { MetaInfo } from "vue-meta/types";
 
 const module: Module = function () {
   if (!this.options.voxes) {
-    console.error('Voxes: Missing Voxes appKey in nuxt config')
-    return
+    console.error("Voxes: Missing Voxes appKey in nuxt config");
+    return;
   }
 
-  const head = this.options.head as MetaInfo
-  const SDK_URL = this.nuxt.options.publicRuntimeConfig.sdkUrl
+  const head = this.options.head as MetaInfo;
+  const SDK_URL =
+    this.nuxt.options.publicRuntimeConfig.sdkUrl || "https://voxes.io/sdk.js";
 
   const script = {
-    id: 'voxessdk',
-    hid: 'voxessdk',
+    id: "voxessdk",
+    hid: "voxessdk",
     src: `${SDK_URL}?appKey=${this.options.voxes.appKey}`,
     async: true,
-  }
+  };
 
   if (head.script) {
-    head.script.push(script)
-    return
+    head.script.push(script);
+    return;
   }
 
-  head.script = [script]
-}
+  head.script = [script];
+};
 
-export default module
-
+export default module;
