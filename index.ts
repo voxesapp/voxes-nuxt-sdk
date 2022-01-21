63148505cc08423d2d3cd2ee89af8eab45520dad
Patryk Gulas <patrykgulas@Glovousers-MacBook-Pro.local>
2022-01-21 16:47:49 +0100

    init commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nuxt/types'
+import { MetaInfo } from 'vue-meta/types'
+
+const module: Module = function () {
+  if (!this.options.voxes) {
+    console.error('Voxes: Missing Voxes appKey in nuxt config')
+    return
+  }
+
+  const head = this.options.head as MetaInfo
+  const SDK_URL = this.nuxt.options.publicRuntimeConfig.sdkUrl
+
+  const script = {
+    id: 'voxessdk',
+    hid: 'voxessdk',
+    src: `${SDK_URL}?appKey=${this.options.voxes.appKey}`,
+    async: true,
+  }
+
+  if (head.script) {
+    head.script.push(script)
+    return
+  }
+
+  head.script = [script]
+}
+
+export default module
+
